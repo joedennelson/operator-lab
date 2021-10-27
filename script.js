c1a131901df7954b5eebf3cf736bc0c2fefb554a
nelsonjm <njoeden05@gmail.com>
2021-10-27 09:41:23 -0700

    im done

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+console.log('hello i am alive');
+
+
+if(7 < 5){
+    console.log('you are correct 7 is less than 5!')
+}
+else{
+    console.log('you are not correct 7 is not less than 5!')
+};
+if(10 < 5){
+    console.log('7 is less than 5');
+}
+else if (1 < 5){
+    console.log('1 is less than 5')
+}
+else{
+    console.log('3 is equal to 3')
+};
+
+
+
+
